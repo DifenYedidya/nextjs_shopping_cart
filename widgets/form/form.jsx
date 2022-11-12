da673a58c42eec9de337a18a9d3bb81b138af995
DifenYedidya <difen.yedidya@gmail.com>
2022-11-12 22:57:15 +0700

    home

---
+++ widgets/form/form.jsx
@@ -0,0 +1,21 @@
+export default function Form() {
+    <form className={styles.form} action="/send-data-here" method="post">
+        <div>
+          <label for="name">Name:</label>
+          <input type="text" id="name" name="name" placeholder='input your name' required minlength="3" maxlength="30"/>
+        </div>
+        <div>
+          <label for="email">Email:</label>
+          <input type="text" id="email" name="email" placeholder='input your email' required />
+        </div>
+        <div>
+          <label for="roll">Phone number:</label>
+          <input type="text" id="phone" name="phone" placeholder='input your phone number' required minlength="10" maxlength="13" />
+        </div>
+        <div>
+          <label for="address">Address:</label>
+          <input type="text" id="address" name="address" placeholder='input your address' required />
+        </div>
+        <button className='button' type="submit">Save</button>
+    </form>
+}
